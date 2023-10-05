e3175dc2412f2e0fe0bab208c0b85a011dabc994
“jbrigham08” <jbrigham08@gmail.com>
2023-10-05 18:43:16 -0500

    full project

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+const app = express();
+
+const dbURI = 'mongodb+srv://jeffblog:Liberty08@jeffblog.w10k0tp.mongodb.net/jeffblog?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
